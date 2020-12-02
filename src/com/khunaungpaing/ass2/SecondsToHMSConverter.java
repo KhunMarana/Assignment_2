b88c41f510bb91e3ff04e355bd0b877154716238
Khun Aung Paing <54024040+KhunMarana@users.noreply.github.com>
2020-12-02 18:18:04 +0630

    Update SecondsToHMSConverter.java

--- src/com/khunaungpaing/ass2/SecondsToHMSConverter.java
+++ src/com/khunaungpaing/ass2/SecondsToHMSConverter.java
@@ -1,21 +1,15 @@
 package com.khunaungpaing.ass2;
+package com.khunaungpaing.ass2;
 
 public class SecondsToHMSConverter {
 
 	public void converter (int seconds) {
-		int hours = 0;
-		int mins = 0;
-		int secs = seconds;
+		int mins = seconds/60;
+		int secs = seconds-(mins*60);
 		
-		while(secs>=60) {
-			mins +=1;
-			secs -=60;
-			
-			while(mins>=60) {
-				hours +=1;
-				mins-=60;
-			}
-		}
+		int hours = mins/60;
+		mins -= (hours*60);
+
 		
 		System.out.println(hours+" h : "+mins+" m : "+secs+" s");
 	}
